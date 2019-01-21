495cc1800895f4aca0778b3c43b00de9ba439fd3
Arunda Victor <victoroyugi31@gmail.com>
2019-01-21 10:17:46 +0300

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GitProfileComponent } from './git-profile/git-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    GitProfileComponent
   ],
   imports: [
     BrowserModule,
